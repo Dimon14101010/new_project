2148e00a817d768910fd01badd94d08dbf079011
SergeyMell <sergey.mell@agilie.com>
2017-07-21 16:40:17 +0300

    Fixes

--- app/modules/venue.module.js
+++ app/modules/venue.module.js
@@ -8,7 +8,7 @@ venueApi.$inject = ['$resource'];
 function venueApi($resource) {
 
     let venueData = {
-        venueValues : $resource('https://api.foursquare.com/v2/venues/explore?v=20131016&ll=48.414118%2C35.126582&section=food&novelty=new&client_id=MCYZZWIG0EVIPENVBVH4TOW3302YFRE5SUCGP0ECVTXG3MBE&client_secret=VBDZI0GECFF24FKACQW12L15JR5VEKUK0RZZTV51QJ0XYY3G')
+        venueValues : $resource('https://api.foursquare.com/v2/venues/explore?v=20131016&venuePhotos=1&ll=48.414118%2C35.126582&section=food&novelty=new&client_id=MCYZZWIG0EVIPENVBVH4TOW3302YFRE5SUCGP0ECVTXG3MBE&client_secret=VBDZI0GECFF24FKACQW12L15JR5VEKUK0RZZTV51QJ0XYY3G')
 
     };
     console.log('srabotalo',venueData)
