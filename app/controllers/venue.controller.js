83eb895ad999739f2987e0ce630b6dba5df6cbc8
Dimon14101010 <dmitriy.nekrasov89>
2017-07-22 19:07:55 +0300

    try get id

--- app/controllers/venue.controller.js
+++ app/controllers/venue.controller.js
@@ -9,18 +9,21 @@ function venueCtrl(venueApi,$geolocation,$scope) {
 
     getLocate();
     function getLocate() {
+
         $geolocation.getCurrentPosition().then(function(location) {
             $scope.mesto = location});
-        $scope.coords = $geolocation.position.coords;
+
+
+
     }
-    console.log('coord',getLocate());
+    console.log('coord', getLocate());
 
 
     let vm = this;
 
     vm.model = {
         venueDataId : [],
-        venueRespone : venueApi.venueValues.get().$promise
+        venueRespone : venueApi.venueValues.get({lat : $geolocation.position.coords , long : $geolocation.position.coords}).$promise
                  .then(function(response) {
                 vm.model.venueRespone = response;
                 vm.model.venueRespone.response.groups[0].items.forEach(function(venue) {venue.venuePhotos = venueApi.venuePhotos.get({id: venue.venue.id});
