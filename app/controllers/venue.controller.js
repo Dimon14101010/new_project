ae2522ad810987e6f824b6d29e02f52898556b50
Dimon14101010 <dmitriy.nekrasov89>
2017-07-22 22:33:45 +0300

    try get id

--- app/controllers/venue.controller.js
+++ app/controllers/venue.controller.js
@@ -7,34 +7,35 @@ venueCtrl.$inject = ['venueApi','$geolocation','$scope'];
 
 function venueCtrl(venueApi,$geolocation,$scope) {
     var position = {};
-
+    let vm = this;
 
     getLocate();
     function getLocate() {
 
         $geolocation.getCurrentPosition().then(function(location) {
             position.lat = location.coords.latitude;
-        position.long = location.coords.longitude});
-        return position;
+        position.long = location.coords.longitude
 
+        vm.model = {
+            venueDataId : [],
+            venueRespone : venueApi.venueValues.get({lat : position.lat , long : position.long}).$promise
+                .then(function(response) {
+                    vm.model.venueRespone = response;
+                    vm.model.venueRespone.response.groups[0].items.forEach(function(venue) {venue.venuePhotos = venueApi.venuePhotos.get({id: venue.venue.id});
 
+                    })})
+
+        };
+        });
+    return vm;
 
     }
     console.log('coord',position);
 
 
-    let vm = this;
 
-    vm.model = {
-        venueDataId : [],
-        venueRespone : venueApi.venueValues.get({lat : position.lat , long : position.long}).$promise
-                 .then(function(response) {
-                vm.model.venueRespone = response;
-                vm.model.venueRespone.response.groups[0].items.forEach(function(venue) {venue.venuePhotos = venueApi.venuePhotos.get({id: venue.venue.id});
 
-                })})
 
-    };
 
 
     console.log ('controller');
--- app/modules/venue.module.js
+++ app/modules/venue.module.js
@@ -8,7 +8,7 @@ venueApi.$inject = ['$resource'];
 function venueApi($resource) {
 
     let venueData = {
-        venueValues : $resource('https://api.foursquare.com/v2/venues/explore?v=20131016&venuePhotos=1&ll=:lat%:long&novelty=new&client_id=I5RTKGWY0YNUAUZL4JWAB22EDSEC4PQF1O4SGKOPZUHEJRS1&client_secret=ZVD4NHQL0RD5QYKSAEO4E2X3ILJ4P2EFQCE5TPHWCGMSRNOM',{lat :'@lat',long: '@long'}),
+        venueValues : $resource('https://api.foursquare.com/v2/venues/explore?v=20131016&venuePhotos=1&ll=:lat,:long&novelty=new&client_id=I5RTKGWY0YNUAUZL4JWAB22EDSEC4PQF1O4SGKOPZUHEJRS1&client_secret=ZVD4NHQL0RD5QYKSAEO4E2X3ILJ4P2EFQCE5TPHWCGMSRNOM',{lat :'@lat',long: '@long'}),
         venuePhotos : $resource('https://api.foursquare.com/v2/venues/:id/photos?v=20131016&client_id=I5RTKGWY0YNUAUZL4JWAB22EDSEC4PQF1O4SGKOPZUHEJRS1&client_secret=ZVD4NHQL0RD5QYKSAEO4E2X3ILJ4P2EFQCE5TPHWCGMSRNOM',{id: '@id'})
 
 
